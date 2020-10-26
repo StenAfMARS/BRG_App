723c695e247b4ec5551f4de4aa08dfe1b29e7579
Simon Søborg <s173255@student.dtu.dk>
2020-10-26 14:56:41 +0100

    Created a shape and started work on first brøg fragment.

--- app/src/main/java/grp02/brg_app/BroegActivity1.java
+++ app/src/main/java/grp02/brg_app/BroegActivity1.java
@@ -2,10 +2,14 @@ package grp02.brg_app;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.LayoutInflater;
 import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
