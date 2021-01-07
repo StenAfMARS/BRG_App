f58378dbbf978bca250c7e142ccd30062e920d2d
Simon Søborg <s173255@student.dtu.dk>
2021-01-07 09:58:28 +0100

    Logic Controller og ScrollChoice

---
+++ app/src/main/java/grp02/brg_app/Control/LogicController.java
@@ -0,0 +1,66 @@
+package grp02.brg_app.Control;
+
+import java.text.DecimalFormat;
+import java.util.ArrayList;
+
+public class LogicController {
+
+    // Singleton class
+    private static LogicController Instance;
+
+    private LogicController() {}
+
+    public static LogicController getInstance() {
+        if (Instance == null) {
+            Instance = new LogicController();
+        }
+
+        return Instance;
+    }
+
+    public float convertStringsToFloats(String str1, String str2) {
+        float val1 = Float.parseFloat(str1);
+        float val2 = Float.parseFloat(str2);
+        float result = val1 + (val2 / 10);
+        System.out.println("This is the converted result: " + result);
+
+        return result;
+    }
+
+    public ArrayList<String> loadMængderIGram(){
+
+        ArrayList<Integer> gramValues = new ArrayList<>();
+
+        for(int i = 50; i <= 75; i++) {
+            gramValues.add(i);
+        }
+
+        return convertToStringList(gramValues);
+    }
+
+    public ArrayList<String> loadMængderIMilliGram(){
+
+        ArrayList<Integer> milliGramValues = new ArrayList<>();
+
+        for(int i = 0; i <= 9; i++) {
+            milliGramValues.add(i);
+        }
+
+        return convertToStringList(milliGramValues);
+    }
+
+    public ArrayList<String> convertToStringList(ArrayList<Integer> list) {
+        ArrayList<String> convertedResults = new ArrayList<>();
+        int n = list.size();
+        if(n == 0) {
+            System.out.println("no items in list");
+            return null;
+        }
+
+        for(int item : list) {
+            convertedResults.add(String.valueOf(item));
+        }
+
+        return convertedResults;
+    }
+}
