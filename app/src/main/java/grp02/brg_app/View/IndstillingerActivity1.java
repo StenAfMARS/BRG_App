adfda100ff9698d489f39adb38e55dfa92955388
Jacob <jacob.ziska@gmail.com>
2021-01-13 13:44:08 +0100

    Fixed colors and progressbar in bryg fragments added Rens fragments added bluetooth fragments

--- app/src/main/java/grp02/brg_app/View/IndstillingerActivity1.java
+++ app/src/main/java/grp02/brg_app/View/IndstillingerActivity1.java
@@ -2,8 +2,10 @@ package grp02.brg_app.View;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
 
 import android.annotation.SuppressLint;
+import android.content.Context;
 import android.content.Intent;
 import android.content.res.ColorStateList;
 import android.graphics.Color;
@@ -14,72 +16,60 @@ import android.widget.Button;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 import grp02.brg_app.R;
+import grp02.brg_app.View.IndstillingerFragments.BluetoothIActivity;
+import grp02.brg_app.View.RensFragments.RensIActivity;
 
-public class IndstillingerActivity1 extends AppCompatActivity implements View.OnClickListener {
+public class IndstillingerActivity1 extends AppCompatActivity {
 
-    Button bt_BTN;
+    @SuppressLint("StaticFieldLeak")
+    public static Context context;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_indstillinger1);
-        bt_BTN = findViewById(R.id.BTN_Bluetooth);
-        // Navigation
-        // ##########################################################
-        BottomNavigationView bottomNav = findViewById(R.id.bottomNav);
-        // Set nav highlighted button
-        bottomNav.setSelectedItemId(R.id.nav_IndstillingerBtn);
-        // Perform ItemSelectedListener
-        bottomNav.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
-            @Override
-            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
-                switch (item.getItemId()) {
-                    case R.id.nav_HomeBtn:
-                        startActivity(new Intent(getApplicationContext(), MainActivity.class));
-                        overridePendingTransition(0, 0);
-                        return true;
-                    case R.id.nav_BroegBtn:
-                        startActivity(new Intent(getApplicationContext(), BroegActivity1.class));
-                        overridePendingTransition(0, 0);
-                        return true;
-                    case R.id.nav_HistorikBtn:
-                        startActivity(new Intent(getApplicationContext(), HistorikActivity1.class));
-                        overridePendingTransition(0, 0);
-                        return true;
-                    case R.id.nav_RensBtn:
-                        startActivity(new Intent(getApplicationContext(), RensActivity1.class));
-                        overridePendingTransition(0, 0);
-                        return true;
-                    case R.id.nav_IndstillingerBtn:
-                        return true;
+
+        context = this;
+
+        if (savedInstanceState == null) {
+            Fragment fragment = new BluetoothIActivity();
+            getSupportFragmentManager().beginTransaction()
+                    .add(R.id.bluetoothFragment, fragment)  // tom container i layout
+                    .commit();
+
+
+            // Navigation
+            // ##########################################################
+            BottomNavigationView bottomNav = findViewById(R.id.bottomNav);
+            // Set nav highlighted button
+            bottomNav.setSelectedItemId(R.id.nav_IndstillingerBtn);
+            // Perform ItemSelectedListener
+            bottomNav.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+                @Override
+                public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                    switch (item.getItemId()) {
+                        case R.id.nav_HomeBtn:
+                            startActivity(new Intent(getApplicationContext(), MainActivity.class));
+                            overridePendingTransition(0, 0);
+                            return true;
+                        case R.id.nav_BroegBtn:
+                            startActivity(new Intent(getApplicationContext(), BroegActivity1.class));
+                            overridePendingTransition(0, 0);
+                            return true;
+                        case R.id.nav_HistorikBtn:
+                            startActivity(new Intent(getApplicationContext(), HistorikActivity1.class));
+                            overridePendingTransition(0, 0);
+                            return true;
+                        case R.id.nav_RensBtn:
+                            startActivity(new Intent(getApplicationContext(), RensActivity1.class));
+                            overridePendingTransition(0, 0);
+                            return true;
+                        case R.id.nav_IndstillingerBtn:
+                            return true;
+                    }
+                    return false;
                 }
-                return false;
-            }
-        });
-        // ##########################################################
-
-        initBtn();
-
-    }
-    public void initBtn(){
-        bt_BTN.setOnClickListener(this);
-    }
-
-    @SuppressLint("ResourceAsColor")
-    @Override
-    public void onClick(View v) {
-        if (v == bt_BTN){
-            if(!(bt_BTN.getBackgroundTintList() == ColorStateList.valueOf(Color.BLUE))) {
-
-                // State Bluetooth Connected!
-                bt_BTN.setBackgroundResource(R.drawable.ic_baseline_bluetooth_audio_24);
-                bt_BTN.setBackgroundTintList(ColorStateList.valueOf(Color.BLUE));
-            } else {
-
-                // State Bluetooth NOT Connected!
-                bt_BTN.setBackgroundResource(R.drawable.ic_baseline_bluetooth_24);
-                bt_BTN.setBackgroundTintList(ColorStateList.valueOf(Color.rgb(139, 90, 57)));
-
-            }
+            });
+            // ##########################################################
         }
     }
 }
