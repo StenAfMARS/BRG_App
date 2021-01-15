b0752db60d0839779099499186648b933be025bc
peter skaarup <peterskaarup@gmail.com>
2021-01-15 10:01:53 +0100

    BLE initail set up

--- app/src/main/java/grp02/brg_app/View/IndstillingerActivity1.java
+++ app/src/main/java/grp02/brg_app/View/IndstillingerActivity1.java
@@ -3,16 +3,30 @@ package grp02.brg_app.View;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.Manifest;
 import android.annotation.SuppressLint;
+import android.app.AlertDialog;
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothGatt;
+import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.content.res.ColorStateList;
 import android.graphics.Color;
+import android.location.LocationManager;
+import android.os.Build;
 import android.os.Bundle;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import grp02.brg_app.Control.BLE.BluetoothController;
 import grp02.brg_app.R;
 
 public class IndstillingerActivity1 extends AppCompatActivity implements View.OnClickListener {
@@ -64,6 +78,10 @@ public class IndstillingerActivity1 extends AppCompatActivity implements View.On
         bt_BTN.setOnClickListener(this);
     }
 
+    private static final int REQUEST_ENABLE_BT = 1;
+    private static final int ACCESS_LOCATION_REQUEST = 2;
+    private BluetoothGatt GATTConnection;
+
     @SuppressLint("ResourceAsColor")
     @Override
     public void onClick(View v) {
@@ -71,8 +89,21 @@ public class IndstillingerActivity1 extends AppCompatActivity implements View.On
             if(!(bt_BTN.getBackgroundTintList() == ColorStateList.valueOf(Color.BLUE))) {
 
                 // State Bluetooth Connected!
-                bt_BTN.setBackgroundResource(R.drawable.ic_baseline_bluetooth_audio_24);
-                bt_BTN.setBackgroundTintList(ColorStateList.valueOf(Color.BLUE));
+                BluetoothController controller = BluetoothController.getInstance();
+                controller.scan();
+                if(controller.scan() != null){
+                    GATTConnection = controller.getConnection();
+                    if(controller.connect(this.getApplicationContext()) != null)
+                    {
+                        
+                        bt_BTN.setBackgroundResource(R.drawable.ic_baseline_bluetooth_audio_24);
+                        bt_BTN.setBackgroundTintList(ColorStateList.valueOf(Color.BLUE));
+                    }
+
+                }else{
+
+                }
+
             } else {
 
                 // State Bluetooth NOT Connected!
@@ -82,4 +113,27 @@ public class IndstillingerActivity1 extends AppCompatActivity implements View.On
             }
         }
     }
+
+    @Override
+    protected void onResume(){
+        super.onResume();
+
+        if (!isBluetoothEnabled()){
+            Intent enableBtIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+            startActivityForResult(enableBtIntent, REQUEST_ENABLE_BT);
+        } else {
+
+        }
+    }
+
+    private boolean isBluetoothEnabled() {
+        BluetoothAdapter bluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
+        if(bluetoothAdapter == null) return false;
+
+        return bluetoothAdapter.isEnabled();
+    }
+
+
+
+
 }
