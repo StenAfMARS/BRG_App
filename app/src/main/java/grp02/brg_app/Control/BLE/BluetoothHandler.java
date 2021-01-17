4a4f4ba133afb0103f16d136bd63d8221d08791f
peter skaarup <peterskaarup@gmail.com>
2021-01-17 21:45:53 +0100

    Added UUIDs

--- app/src/main/java/grp02/brg_app/Control/BLE/BluetoothHandler.java
+++ app/src/main/java/grp02/brg_app/Control/BLE/BluetoothHandler.java
@@ -1,10 +1,22 @@
 package grp02.brg_app.Control.BLE;
 
+import java.util.UUID;
+
 public class BluetoothHandler {
-    private static BluetoothHandler single_instance = null;
+
+    private static volatile BluetoothHandler single_instance = null;
+
+    //UUIDs for ESP32
+    private static final UUID ESP32_SERVICE_UUID = UUID.fromString("4fafc201-1fb5-459e-8fcc-c5c9c331914b");
+    private static final UUID ESP32_CHARACTERISTIC_UUID = UUID.fromString("beb5483e-36e1-4688-b7f5-ea07361b26a8");
 
 
-    private BluetoothHandler(){}
+
+
+
+    private BluetoothHandler(){
+
+    }
 
     public static BluetoothHandler getInstance(){
         if (single_instance == null){
